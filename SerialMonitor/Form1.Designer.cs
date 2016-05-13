763f11dd87a3511dd412b6730e05fafd0cf885dc
bobby <bobbyg603@gmail.com>
2016-05-13 11:10:45 -0400

    hello world!

---
+++ SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.comPortTextBox = new System.Windows.Forms.TextBox();
+            this.recievedBytesTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(13, 13);
+            this.txtMain.Multiline = true;
+            this.txtMain.Name = "txtMain";
+            this.txtMain.ReadOnly = true;
+            this.txtMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMain.Size = new System.Drawing.Size(679, 355);
+            this.txtMain.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 374);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 25);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(93, 374);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 25);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(174, 374);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 25);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status:";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(572, 376);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(120, 20);
+            this.txtStatus.TabIndex = 5;
+            this.txtStatus.Text = "Stopped";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.ReceivedBytesThreshold = 9;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaud.Location = new System.Drawing.Point(393, 376);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(75, 21);
+            this.comboBoxBaud.TabIndex = 7;
+            this.comboBoxBaud.SelectedIndexChanged += new System.EventHandler(this.comboBoxBaud_SelectedIndexChanged);
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Items.AddRange(new object[] {
+            "Master",
+            "Slave"});
+            this.comboBoxDevice.Location = new System.Drawing.Point(312, 376);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(75, 21);
+            this.comboBoxDevice.TabIndex = 8;
+            this.comboBoxDevice.SelectedIndexChanged += new System.EventHandler(this.comboBoxDevice_SelectedIndexChanged);
+            // 
+            // comPortTextBox
+            // 
+            this.comPortTextBox.Location = new System.Drawing.Point(255, 377);
+            this.comPortTextBox.Name = "comPortTextBox";
+            this.comPortTextBox.Size = new System.Drawing.Size(51, 20);
+            this.comPortTextBox.TabIndex = 9;
+            this.comPortTextBox.Text = "COM3";
+            // 
+            // recievedBytesTextBox
+            // 
+            this.recievedBytesTextBox.Location = new System.Drawing.Point(474, 376);
+            this.recievedBytesTextBox.Name = "recievedBytesTextBox";
+            this.recievedBytesTextBox.Size = new System.Drawing.Size(51, 20);
+            this.recievedBytesTextBox.TabIndex = 10;
+            this.recievedBytesTextBox.Text = "9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 411);
+            this.Controls.Add(this.recievedBytesTextBox);
+            this.Controls.Add(this.comPortTextBox);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.comboBoxBaud);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Newayz Serial Port Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.ComboBox comboBoxDevice;
+        private System.Windows.Forms.TextBox comPortTextBox;
+        private System.Windows.Forms.TextBox recievedBytesTextBox;
+    }
+}
+
---
+++ SerialMonitor/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+
+namespace SerialMonitor
+{
+    public partial class Form1 : Form
+    {        
+        public const int indexMaster = 0;
+        public const int indexSlave = 1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxDevice.SelectedIndex = 1;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string comPort = comPortTextBox.Text;
+            string recievedBytesThreshold = recievedBytesTextBox.Text;
+
+            if (!Regex.IsMatch(comPort, "COM[0-9]{1,2}"))
+            {
+                AppendTextToForm("Please enter a valid COM port!");
+                return;
+            }
+
+            try
+            {
+                serialPort1.PortName = comPort;
+                serialPort1.ReceivedBytesThreshold = int.Parse(recievedBytesThreshold);
+            }
+            catch (Exception ex)
+            {
+                AppendTextToForm(ex.ToString());
+                return;
+            }
+
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                    txtStatus.Text = "Running";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    AppendTextToForm(ex.ToString());
+                }
+            }
+            else
+                AppendTextToForm("Already running!");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    txtStatus.Text = "Stopped";
+                    btnStop.Enabled = false;
+                    btnStart.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    AppendTextToForm(ex.ToString());
+                }
+            }
+            else
+                AppendTextToForm("Already stopped!");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMain.Text = String.Empty;
+        }
+
+        // Used to update the status textbox on the MainForm from another thread
+        private delegate void ControlStringConsumer(TextBox textbox, string text);
+
+        private void UpdateTextBoxOnMainForm(TextBox textbox, string text)
+        {
+            if (textbox.InvokeRequired)
+            {
+                textbox.Invoke(new ControlStringConsumer(UpdateTextBoxOnMainForm), new object[] { textbox, text });
+            }
+            else
+            {
+                textbox.AppendText(text + Environment.NewLine);
+            }
+        }
+        
+        private void AppendTextToForm(string text)
+        {
+            Debug.WriteLine("info received");
+            UpdateTextBoxOnMainForm(txtMain, text + Environment.NewLine);
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            int length = sp.BytesToRead;
+            byte[] inData = new byte[length];
+
+            sp.Read(inData, 0, length);
+
+            AppendTextToForm(BitConverter.ToString(inData));
+        }
+
+        private void comboBoxBaud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBoxBaud.SelectedIndex;
+            int baud = 9600;
+
+            switch (index)
+            {
+                case 0: baud = 2400; break;
+                case 1: baud = 4800; break;
+                case 2: baud = 9600; break;
+                case 3: baud = 14400; break;
+                case 4: baud = 19200; break;
+                case 5: baud = 38400; break;
+                case 6: baud = 57600; break;
+                case 7: baud = 115200; break;
+            }
+
+            try
+            {
+                serialPort1.BaudRate = baud;
+            }
+            catch (Exception ex)
+            {
+                AppendTextToForm(ex.ToString());
+            }
+        }
+
+        private void comboBoxDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxDevice.SelectedIndex == indexMaster)
+            {
+                serialPort1.ReceivedBytesThreshold = 3;
+                comboBoxBaud.SelectedIndex = 7; // TODO use const
+            }
+
+            if (comboBoxDevice.SelectedIndex == indexSlave)
+            {
+                serialPort1.ReceivedBytesThreshold = 9;
+                comboBoxBaud.SelectedIndex = 2; // TODO use const
+            }
+        }
+    }
+}
